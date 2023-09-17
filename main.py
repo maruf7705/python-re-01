6be6831a462e8f12d2173c5dd975ffd16ab5a0c1
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-17 06:59:38 +0600

    Done Join
    Join

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # Write your code below this line 👇
 print("Hello world")
 input("What is your Name ?")
+### NEXT #####
+
+# Write your code below this line 👇
+print("Hello world")
+print("My name is Maruf")
+#next
+#input("What is your name ?")
+print("Maruf \nIslam \nSaad")
+#Next
+input("Hello" + " " + input("what is your color ?"))
+print("Maruf" + " Islam")
