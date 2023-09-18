a9923d3b8fe45b997e8264913905be2b7ca2da19
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-18 22:03:49 +0600

    Ok
    Boss

---
+++ test2.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + b)
+print("b: " + a)
+
+
+### SLOVED DONE ###
+### SLOVED DONE ###
+### SLOVED DONE ###
+### SLOVED DONE ##
+### SLOVED DONE ###
+print("### SLOVED DONE ###")
