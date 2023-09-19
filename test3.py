1bdc8b97b1dda08946e0f8f436751cc1ae438e3d
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-19 22:07:38 +0600

    That is right

---
+++ test3.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+a= (int(two_digit_number [0]))
+b= (int(two_digit_number [1]))
+print(a+b)
+####################################
+#Write your code below this line 👇
