a038b3648ce4e8736961135c7f4168fe596df755
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-19 22:09:01 +0600

    Run my code and add
    I was learn , How to make a small digit calculator

--- test3.py
+++ test3.py
@@ -1,8 +1,22 @@
-# 🚨 Don't change the code below 👇
-two_digit_number = input("Type a two digit number: ")
-# 🚨 Don't change the code above 👆
-a= (int(two_digit_number [0]))
-b= (int(two_digit_number [1]))
-print(a+b)
-####################################
-#Write your code below this line 👇
+# # Data Types
+# # String
+# print("Hello"[4])
+
+# Name_Coun = len(input("what is your name ?"))
+# Name_Coun = str(Name_Coun)
+# print(Name_Coun)
+# a = str(1233)
+# print(type(a))
+
+# num_count = len((input("What is your name ?")))
+# print(type(num_count))
+# #Chek it what happent in this code
+# num_count = str(num_count)
+# print(type(num_count))
+# print("your name  " + num_count + " Is count number ")
+
+two_digit_number = (input("Input your number\n"))
+int(two_digit_number)
+a = int(two_digit_number[0])
+b = int(two_digit_number[1])
+print(a + b)
