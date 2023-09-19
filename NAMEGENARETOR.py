768c4e26370965a66f41b459c60da62fe51c5238
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-19 14:23:15 +0600

    MAKE A NEW
    OK dome

---
+++ NAMEGENARETOR.py
@@ -0,0 +1,5 @@
+print("welcome to the Brand Name Generator.")
+City = (input("what's name of city grow up in ?\n"))
+pet = (input("What's your pet's name?\n"))
+print("Your brand name could be " + City + " " + pet)
+
