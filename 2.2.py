e42ee8b0e8728747734bf37a121ae092329a7305
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-20 19:53:41 +0600

    BMI

---
+++ 2.2.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+height= (float(height))
+weight= (float(weight))
+a = ((weight)/(height * height))
+print (int(a))
+#Write your code below this line 👇
