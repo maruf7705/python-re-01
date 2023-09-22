d71b0cbbdf98a85a1c595b4f437dd3f704dbd8b9
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-22 18:45:04 +0600

    Something Chenage
    Ok

--- 90years.age-calculator.py
+++ 90years.age-calculator.py
@@ -1,8 +1,4 @@
-# 🚨 Don't change the code below 👇
 age = input("What is your current age? ")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 age = int(age)
 days= (age*365)
 days= (90*365)-days
