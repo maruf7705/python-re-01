46d4c429b592673300524fcada5230ac5ed474df
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-22 12:37:32 +0600

    Ya - Lot

--- NEXT.py
+++ NEXT.py
@@ -1,5 +1,14 @@
-Maruf = 50
-sadik = 90
-Saad = 79
+# Maruf = 50
+# sadik = 90
+# Saad = 79
 
-print(f"your photo price is {Maruf}  Now pay it fast")
+# print(f"your photo price is {Maruf}  Now pay it fast")
+
+Football = 50
+cricket = 100
+Winrate = 50
+total_score = (Football + cricket + Winrate)
+total_score += 500
+print(f"your total score is {total_score} ")
+input("isn't it ? \n")
+print("you are correct")
