5dcca5f9d447dd513eaa2d80354b70505e17824a
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-22 19:12:26 +0600

    Update Data
    Update Your Main Data

---
+++ 90years.age-calculator.py
@@ -0,0 +1,8 @@
+age = input("What is your current age? ")
+age = int(age)
+age = 90-age
+days= (age*365)
+week = (age*52)
+month = (age*12 )
+result = (f"You have {days} days, {week} weeks, and {month} months left.")
+print(result)
