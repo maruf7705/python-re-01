8240a03d51027ec3f6ac487c7492afed24b65d5b
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-22 12:26:42 +0600

    Update
    Yea , Update

---
+++ excer.py
@@ -0,0 +1,17 @@
+# #Data Types
+print(round(8 / 3,5))
+print(8 // 3)
+print(type(8 / 3))
+maruf= 45
+print('Maruf')
+
+a = (8 // 4)
+a += 30
+print(a)
+
+score = 10
+height = 20
+
+print(f"your score is {score}")
+
+print(int("50")+50)
